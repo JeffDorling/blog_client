418af55a752822ab8d4f65eb0a555ca963a29db8
jdorling <jdorling15@gmail.com>
2019-11-20 11:53:46 +1000

    Updates

---
+++ precache-manifest.764bdaf4911cc7d5088909d26944db06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fdee642482750de2d6f196422c5b2c",
+    "url": "/blog_client/index.html"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog_client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog_client/static/js/2.0a48bea9.chunk.js"
+  },
+  {
+    "revision": "836d52020a9e585c0fc4",
+    "url": "/blog_client/static/js/main.42d2d035.chunk.js"
+  },
+  {
+    "revision": "cd1f0cedcb6d0bd3518e",
+    "url": "/blog_client/static/js/runtime-main.f60aedc0.js"
+  }
+]);
